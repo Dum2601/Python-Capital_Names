b9191138365b5bb93621f042074725467c2fd3d3
Douglas Barbosa <Doug_b_souza@hotmail.com>
2025-05-01 19:05:46 -0300

    Function finished

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ from utils.data_base.database_queries import name_list
 
 
 name_list = name_list()
+# print(name_list)
 
-print(name_list)
+def capital_letters():
+    fixed_names = []
+    for names in name_list:
+        names_modified = names.title()
+        fixed_names.append(names_modified)
+    return fixed_names
+
+
+print(capital_letters())
--- utils/data_base/database_queries.py
+++ utils/data_base/database_queries.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-con = sqlite3.connect(os.path.abspath('utils/data_base/nomes.db'))
+con = sqlite3.connect(os.path.abspath('utils/data_base/nomes.db')) # He's absolute to avoid the problem of made another database in main directory
 
 cursor = con.cursor()
 
