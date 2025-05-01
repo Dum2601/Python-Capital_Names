94c8614ce0279d6ccdd793947c28fdbef67c16a6
Douglas Barbosa <Doug_b_souza@hotmail.com>
2025-05-01 17:44:31 -0300

    Insert queries

--- utils/data_base/database_management.py
+++ utils/data_base/database_management.py
@@ -6,4 +6,19 @@ con = sqlite3.connect('nomes.db')
 cursor = con.cursor()
 
 
-cursor.execute('CREATE TABLE names (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL)')
+# cursor.execute('CREATE TABLE names (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL)')
+
+
+names = [
+    ("Douglas",),
+    ("Maria",),
+    ("Joao",),
+    ("Carla",),
+    ("Lucas",),
+    ("Fernanda",),
+    ("Rafael",),
+    ("Beatriz",)
+]
+
+# cursor.executemany('INSERT INTO names (nome) VALUES (?)', names)
+# con.commit()
